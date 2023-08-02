ff3a77746b60c8e558f005b155bca4154de8331b
Your Name <you@example.com>
2023-08-02 20:35:01 +0300

    create feature book method

--- lib/feature/home/pressention/manager/feature_book_cubit/featurebook_cubit.dart
+++ lib/feature/home/pressention/manager/feature_book_cubit/featurebook_cubit.dart
@@ -1,9 +1,22 @@
-import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:bookly_app/feature/home/data/model/book_model/book_model.dart';
+import 'package:bookly_app/feature/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'featurebook_state.dart';
 
 class FeaturebookCubit extends Cubit<FeaturebookState> {
-  FeaturebookCubit() : super(FeaturebookInitial());
+  FeaturebookCubit(this.homeRepo) : super(FeaturebookInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchFeatureBook() async {
+    var result = await homeRepo.fetchFeatureBook();
+
+    result.fold((Failure) {
+      emit(FeaturebookFailure(Failure.errmassege));
+    }, (books) {
+      emit(FeaturebookSuccess(books));
+    });
+  }
 }
