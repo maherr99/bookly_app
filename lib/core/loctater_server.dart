119c1343867983482704ad1e20c376a2636ae3f9
Your Name <you@example.com>
2023-08-02 22:25:54 +0300

    create locte server using get_it

---
+++ lib/core/loctater_server.dart
@@ -0,0 +1,13 @@
+import 'package:bookly_app/core/api_services.dart';
+import 'package:bookly_app/feature/home/data/repos/home_repo_impl.dart';
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+
+void setup() {
+  getIt.registerSingleton<ApiServices>(ApiServices(Dio()));
+  getIt.registerSingleton<HomeRepoImpl>(HomeRepoImpl(
+    getIt.get<ApiServices>(),
+  ));
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:bookly_app/constant/constant.dart';
+import 'package:bookly_app/core/api_services.dart';
 import 'package:bookly_app/core/app_route.dart';
+import 'package:bookly_app/core/loctater_server.dart';
+import 'package:bookly_app/feature/home/data/repos/home_repo_impl.dart';
+import 'package:bookly_app/feature/home/pressention/manager/feature_book_cubit/featurebook_cubit.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'feature/home/pressention/manager/cubit/newsetbook_cubit.dart';
 
 void main() {
   runApp(const BooklyApp());
@@ -11,15 +18,29 @@ class BooklyApp extends StatelessWidget {
   const BooklyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: kPrimaryColor,
-        textTheme: GoogleFonts.montserratTextTheme(
-          ThemeData.dark().textTheme,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => FeaturebookCubit(
+            getIt.get<HomeRepoImpl>(),
+          ),
         ),
+        BlocProvider(
+          create: (context) => NewsetbookCubit(
+            getIt.get<HomeRepoImpl>(),
+          ),
+        ),
+      ],
+      child: MaterialApp.router(
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: kPrimaryColor,
+          textTheme: GoogleFonts.montserratTextTheme(
+            ThemeData.dark().textTheme,
+          ),
+        ),
+        debugShowCheckedModeBanner: false,
+        routerConfig: AppRouter.route,
       ),
-      debugShowCheckedModeBanner: false,
-      routerConfig: AppRouter.route,
     );
   }
 }
